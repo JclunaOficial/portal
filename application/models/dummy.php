699f3193c2231253b9f88824f6d69f6605edea60
Juan C. Luna H <jc.luna@live.com.mx>
2016-05-30 18:19:15 -0500

    complementando la estructura del folder de aplicación

---
+++ application/models/dummy.php
@@ -0,0 +1,5 @@
+<?php
+
+final class DummyModel extends Model {
+    
+}
--- system/controller.php
+++ system/controller.php
@@ -14,4 +14,16 @@ class Controller {
     public function loadView($viewName, $navbar = true) {
         return new View($viewName, $navbar);
     }
+    
+    public function loadPlugin($pluginName) {
+        require_once(APP_DIR . 'plugins' . DS . strtolower($pluginName) . '.php');
+    }
+    
+    public function loadClass($className) {
+        require_once(APP_DIR . 'classes' . DS . strtolower($className) . '.php');
+    }
+    
+    public static function navigate($location) {
+        header('Location: ' . BASE_URL . $location);
+    }
 }
