98d6edb63aaa35523dbdf0cd5cd35ff258d4c134
jcluna <jc.luna@live.com.mx>
2016-06-04 23:24:15 -0500

    código para habilitar la independencia de sesión por portal al asignar un valor unico en la constante PORTAL_SESSION

--- application/helpers/session.php
+++ application/helpers/session.php
@@ -26,8 +26,8 @@ final class Session {
         }
 
         $sessionValues = self::getValues();
-        if (array_key_exists($search, $sessionValues)) {
-            return $sessionValues[$search];
+        if (array_key_exists(PORTAL_SESSION . $search, $sessionValues)) {
+            return $sessionValues[PORTAL_SESSION . $search];
         }
         return $default;
     }
@@ -36,22 +36,34 @@ final class Session {
     * Asigna el valor de una variable de sesión
     */
     public static function set($key, $value) {
-        $_SESSION[UString::toUpper($key)] = $value;
+        $_SESSION[UString::toUpper(PORTAL_SESSION . $key)] = $value;
     }
     
     /**
     * Remueve una variable de la sesión
     */
     public static function remove($key) {
-        unset($_SESSION[UString::toUpper($key)]);
+        unset($_SESSION[UString::toUpper(PORTAL_SESSION . $key)]);
     }
     
     /**
     * Elimina todas las variables de la sesión
     */
     public static function destroy() {
-        session_unset();
-        session_destroy();
+        if(PORTAL_SESSION == '') {
+            // destruir todas las variables           
+            session_unset();
+            session_destroy();
+        } else {
+            // localizar y remover las variables especificas del portal
+            $values = self::getValues();
+            foreach ($values as $key => $value) {
+                if(UString::startsWith($key, PORTAL_SESSION)) {
+                    // quitar la variable localizada
+                    unset($_SESSION[$key]);
+                }
+            }
+        }
     }
 
 }
--- application/settings.php
+++ application/settings.php
@@ -6,6 +6,7 @@ define('CONTROLLER_DEFAULT', 'home');
 define('CONTROLLER_ERROR', 'error');
 
 define('PORTAL_TITLE', 'Portal');
+define('PORTAL_SESSION', 'PS_');
 define('PORTAL_NAME', 'Portal');
 define('PORTAL_VERSION', '1.0');
 define('PORTAL_COPYRIGHT', '&copy; 2016 - Empresa Dummy, S.A. de C.V.');
