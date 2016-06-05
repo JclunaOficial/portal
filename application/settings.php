6c31c493fa35cbd610314c6c50f2a05b66777633
jcluna <jc.luna@live.com.mx>
2016-06-04 21:19:03 -0500

    agregar la configuración base para la conexión a datos desde la clase Model

--- application/settings.php
+++ application/settings.php
@@ -8,3 +8,7 @@ define('PORTAL_TITLE', 'Portal');
 define('PORTAL_NAME', 'Portal');
 define('PORTAL_VERSION', '1.0');
 define('PORTAL_COPYRIGHT', '&copy; 2016 - Empresa Dummy, S.A. de C.V.');
+
+define('DB_DSN', 'mysql:host=localhost;dbname=portal;');
+define('DB_USR', 'root');
+define('DB_PWD', '');
